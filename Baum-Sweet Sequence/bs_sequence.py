bf104991329b302077e8fea260e496890950db3b
Tomasz Koscielniak <tomek.maq@gmail.com>
2018-03-26 20:40:13 +0200

    Kaprekar Numbers

--- Baum-Sweet Sequence/bs_sequence.py
+++ Baum-Sweet Sequence/bs_sequence.py
@@ -5,7 +5,7 @@ def bs_sequence(range_nr):
     split_numbers = list(
         list(filter(lambda x: x != '', bin(number)[2:].split("1"))) for number in range(1, range_nr + 1))
     for item in split_numbers:
-        item = [False for (element) in item if divmod(len(element), 2)[1] != 0]
+        item = [False for element in item if divmod(len(element), 2)[1] != 0]
         if False in item:
             result.append(0)
         else:
